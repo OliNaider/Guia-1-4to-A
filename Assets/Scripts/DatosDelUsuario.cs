701bea78278a12f06abad852fb31fd2cb5d142ba
OliNaider <166539084+OliNaider@users.noreply.github.com>
2025-03-25 12:12:09 -0300

    Debuging las variables

--- Assets/Scripts/DatosDelUsuario.cs
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -14,7 +14,8 @@ public class DatosDelUsuario : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Mi nombre es " + nombreUsuario "tengo " + String(edadUsuario) );
+         
     }
 
     // Update is called once per frame
