c5a0b6f978f624582672b61561c13f6e2a11cac8
OliNaider <166539084+OliNaider@users.noreply.github.com>
2025-04-08 12:12:38 -0300

    de que orientacion sos

---
+++ Assets/Scripts/DeQueOrientacionSos.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeQueOrientacionSos : MonoBehaviour
+{
+    public string nombreAlumno;
+    public int añoAlumno;
+    public string orientacionAlumno;
+
+    string tic = "T";
+    string diseño = "D";
+    string humanidades = "H";
+    string gestion = "G";
+    string medios = "M";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (nombreAlumno == "")
+        {
+            Debug.Log("Error. Ingresar nombre");
+            return;
+        }
+
+        if (añoAlumno < 1 || añoAlumno > 5)
+        {
+            Debug.Log("El año ingresado no está en el rango apropiado");
+            return;
+        } else if(añoAlumno < 3 || añoAlumno > 5)
+        {
+            Debug.Log("Error.Aún estás en el ciclo básico");
+            return;
+        }
+
+        if(orientacionAlumno != tic && orientacionAlumno != diseño && orientacionAlumno != humanidades && orientacionAlumno != gestion && orientacionAlumno != medios)
+        {
+            Debug.Log("valor no valido");
+            return;
+        }
+
+        Debug.Log("Muchas gracias " + nombreAlumno + "!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
